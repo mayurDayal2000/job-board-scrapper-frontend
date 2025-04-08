789fb9ee6073607dc737866cd89c66a46cb18108
Mayur Dayal <mayur.dayal97@gmail.com>
2025-04-08 22:48:40 +0530

    feat(app): added dark theme toggler with shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { ThemeToggler } from "@/components/theme-toggler";
 
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
       <Button variant="outline">Click me</Button>
+
+      <ThemeToggler />
     </>
   );
 }
